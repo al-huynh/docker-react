61fd9f3ee9706a1238f454ad570b8d741f00da6b
alan <alanhuynh43@gmail.com>
2024-02-07 14:10:20 -0500

    fixed test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bonk Bonk Bonk!
+          learn react
         </a>
       </header>
     </div>
